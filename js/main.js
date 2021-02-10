c0a6c4196e9f9eb24c89940a6d6803a455d391d5
bgcrafts <bartosz.giecewicz@gmail.com>
2021-02-10 19:12:13 +0100

    zadanie33

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let p = document.querySelector('p');
+let aha = document.getElementById("opis");
+let serio = document.querySelector('button');
+serio.onclick = function () {
+    p.innerText = "akuku";
+}
